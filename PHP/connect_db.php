45e38197faa840febfcf9904034cd5dc9fcd80e0
= <alishabraiz154@gmail.com>
2025-11-22 21:42:11 -0800

    DataBase fixing

--- PHP/connect_db.php
+++ PHP/connect_db.php
@@ -1,6 +1,6 @@
 <?php 
-$connection = new mysqli('localhost','root','','pizzacoperation')or die("Could not connect to mysql".mysqli_error($con));
-if(!$connection){
+// $connection = new mysqli('localhost','root','','pizzacoperation')or die("Could not connect to mysql".mysqli_error($con));
 $connection = new mysqli('151.106.122.2','u694959030_pizzaman','h0Ji25UQ>','u694959030_pizzass')or die("Could not connect to mysql".mysqli_error($con));
-}
+// if(!$connection){
+// }
 ?>
